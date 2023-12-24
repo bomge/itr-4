7df93d928ba428b221518f2b1f0980be734fd868
Uladzislau <a@a.a>
2023-12-24 16:05:26 +0300

    some fixes

--- backend/server.js
+++ backend/server.js
@@ -17,15 +17,15 @@ const corsConfig = {
   optionsSuccessStatus: 200,
 }
 
-app.use(credentials)
+// app.use(credentials)
 app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://resonant-faun-ce409d.netlify.app/");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Credentials", "true"); // Allow credentials
+  res.header("Access-Control-Allow-Origin", "https://resonant-faun-ce409d.netlify.app"); 
+  // specify origin 
+  res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
 
